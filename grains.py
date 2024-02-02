9e1f0ec6f71c970bb1cf53ff5ae84dd24869fce1
striderwalk <striderwalk@gmail.com>
2024-02-02 16:37:34 +0000

    fixed type seletion

--- grains.py
+++ grains.py
@@ -8,3 +8,5 @@ with open("./assets/types.json") as file:
 
     for key, value in GRAIN_DATA.items():
         GRAIN_DATA[key] = (value[0], np.array(value[1]), value[2])
+
+GRAINS = GRAIN_DATA.keys()
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import random
-
 import pygame
 
 from box import Box
 from consts import *
-from mouse import Mouse_hander
+from inputs import InputHandler
 
 
 def main():
@@ -15,33 +13,16 @@ def main():
 
     # setup grid
     box = Box()
-    mouse_handler = Mouse_hander()
-
     box.place_grains((10, 50), (30, 74), "air")
+    input_handler = InputHandler()
 
     # main loop -------------------------------->
-
-    for i in range(1000):
-        # while True:
+    # for i in range(1000):
+    while True:
         box.update(win)
-        mouse_handler.update(win, box)
-
-        # handle events -------------------------------->
-        for event in pygame.event.get():
-            # quit event
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    box.reset()
-
-                elif event.key == pygame.K_f:
-                    box.check_chunks()
-
-            elif event.type == pygame.MOUSEWHEEL:
-                mouse_handler.scale_cursor(event.precise_y)
+        if input_handler.update(win, box):
+            pygame.quit()
+            return
 
         # update frame
         pygame.display.flip()
